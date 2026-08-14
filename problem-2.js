7d53dd306525eee83837164cababbbbe6946b6c4
my first github repo <mrarifmondol98@gmail.com>
2026-08-14 15:41:33 +0600

    es6 problem solving

---
+++ problem-2.js
@@ -0,0 +1,14 @@
+
+const calculateBMI = (weight, height) =>{
+    if(weight <= 0 || height <= 0 || typeof weight !== 'number' || typeof height !== 'number'){
+        return "Invalid"
+    }
+
+    let BMI = weight / (height *height)
+    let result = Number(BMI.toFixed(2))
+    return result;
+}
+
+console.log(calculateBMI(70, 1.75));
+console.log(calculateBMI(50, 1.6));
+console.log(calculateBMI(60, -1.7));
